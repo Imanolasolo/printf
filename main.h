8ea58548268dd84a6fc990dc5b8dafe98804a343
Bryan Freire <bryanalexfreire@gmail.com>
2021-11-07 10:41:28 -0800

    main.h

--- main.h
+++ main.h
@@ -3,7 +3,11 @@
 #define <starg.h>
 #define <stdio.h>
 #define <stdlib.h>
-
+typedef struct print
+{
+	char *t;
+	int(*f)(va_list);
+}print_t;
 int _putchar(char c);
 int _printf(const char *format, ...);
 #endif
