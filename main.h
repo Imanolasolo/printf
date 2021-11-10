f0a6203d07a042e331d2b98cb08111351eb5ba1b
Imanolasolo <jjusturi@gmail.com>
2021-11-10 08:34:59 -0500

    main.h

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_H         /* Define MAIN_H */
 
-#include<stdarg.h>
+#include <stdarg.h>    /* Define stdargh library */
 /**
  * struct print - structure for printing various types
  * @t: type to print
@@ -9,13 +9,24 @@
  */
 typedef struct print
 {
-    /* data */
-char *t;
-int (*f)(va_list);
+	char *t;
+	int (*f)(va_list);
 } print_t;
-
-/* functions to use */
+/* List of functions*/
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-endif /* MAIN_H */
+#endif
